2e57ce796995c70c111782a7c09f03cd86f2cf87
DylanPerry5@gmail.com <DylanPerry5@gmail.com>
2019-09-02 14:37:11 -0400

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Dylan Perry on 9/2/19.
+//  Copyright © 2019 Dylan Perry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
