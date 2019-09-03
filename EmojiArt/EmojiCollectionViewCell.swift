56b0376f438dde921db7f38440b2b8f992e92809
DylanPerry5@gmail.com <DylanPerry5@gmail.com>
2019-09-03 14:47:06 -0400

    Finishing up lesson 12 commits

---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Dylan Perry on 9/3/19.
+//  Copyright © 2019 Dylan Perry. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
